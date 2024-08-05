b270bef807ed718daa77791b4d748420eb55828f
Ara404E <119130650+Ara404E@users.noreply.github.com>
2024-08-05 12:32:41 +0300

    wbesite now is dynamic

---
+++ src/home.js
@@ -0,0 +1,6 @@
+import {currentTabDiv} from "./index";
+
+
+export function displayHome(){
+    return;
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import _ from 'lodash';
 import './style.css';
+import './home';
 
+import {displayHome} from './home'
+
+const homeTab=document.querySelector('#home-tab');
+const todayTab=document.querySelector('#today-tab');
+const ThisWeek=document.querySelector('#thisWeek-tab');
+export const currentTabDiv=document.querySelector(".current-tab-div")
+
+
+homeTab.addEventListener('click', ()=>{
+  currentTabDiv.textContent="";
+});
+
+todayTab.addEventListener('click', ()=>{
+    currentTabDiv.textContent="";
+
+});
+
+ThisWeek.addEventListener('click', ()=>{
+        currentTabDiv.textContent="";
+})
+
+displayHome();
