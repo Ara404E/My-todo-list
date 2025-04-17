58f3460463b0dc6395c416f777d1c8063a25b3f8
Ara404E <arasrwsht.03@gmail.com>
2025-04-17 18:04:05 +0300

    Publishing website

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,34 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-   mode: 'development',
+  mode: 'development',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean : true
+    clean: true,
+    publicPath: '/My-todo-list/', // Critical for GitHub Pages
   },
-    module: {
+  module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-       {
+      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
-           {
+      {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
     ],
+  },
+  devServer: { // Optional but recommended
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
   },
 };
